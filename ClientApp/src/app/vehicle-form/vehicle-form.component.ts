deea38ea6137364a2a40d50364943f3af8271e9d
John Sourvinos <johnsourvinos@hotmail.com>
2019-04-24 12:20:40 +0300

    Populates radio buttons and checkboxes

--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -1,23 +1,23 @@
-import { KeyValuePair } from './../models/keyValuePair';
-import * as _ from 'underscore';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { SaveVehicle } from '../models/savevehicle';
-import { Vehicle } from './../models/vehicle';
-import { VehicleService } from './../services/vehicle.service';
-import { forkJoin } from 'rxjs';
+import * as _ from 'underscore'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Component, OnInit } from '@angular/core'
+import { KeyValuePair } from './../models/keyValuePair'
+import { SaveVehicle } from '../models/savevehicle'
+import { Vehicle } from './../models/vehicle'
+import { VehicleService } from './../services/vehicle.service'
+import { forkJoin } from 'rxjs'
 
 @Component({
     selector: 'app-vehicle-form',
     templateUrl: './vehicle-form.component.html',
-    styleUrls: ['./vehicle-form.component.css']
+    styleUrls: ['./vehicle-form.component.css'],
 })
 
 export class VehicleFormComponent implements OnInit {
 
-    makes: any;
-    models: any;
-    features: any;
+    makes: any
+    models: any
+    features: any
 
     vehicle: SaveVehicle = {
         id: 0,
@@ -25,65 +25,82 @@ export class VehicleFormComponent implements OnInit {
         modelId: 0,
         isRegistered: false,
         features: [],
-        contact: { name: '', email: '', phone: '' }
-    };
+        contact: { name: '', email: '', phone: '' },
+    }
 
     constructor(private route: ActivatedRoute, private router: Router, private vehicleService: VehicleService) {
-        route.params.subscribe(p => this.vehicle.id = +p['id']);
+        route.params.subscribe(p => (this.vehicle.id = +p['id']))
     }
 
     ngOnInit() {
         let sources = [
             this.vehicleService.getMakes(),
-            this.vehicleService.getFeatures()
-        ];
+            this.vehicleService.getFeatures(),
+        ]
 
         if (this.vehicle.id) {
-            sources.push(this.vehicleService.getVehicle(this.vehicle.id));
+            sources.push(this.vehicleService.getVehicle(this.vehicle.id))
         }
 
-        return forkJoin(sources).subscribe(result => {
-            this.makes = result[0];
-            this.features = result[1];
-            if (this.vehicle.id) {
-                this.setVehicle(result[2] as { id: number, make: KeyValuePair, model: KeyValuePair, isRegistered: boolean, features: [], contact: { name: string, email: string, phone: string }, lastUpdate: string });
+        return forkJoin(sources).subscribe(
+            result => {
+                this.makes = result[0]
+                this.features = result[1]
+                if (this.vehicle.id) {
+                    this.setVehicle(result[2] as {
+                        id: number
+                        make: KeyValuePair
+                        model: KeyValuePair
+                        isRegistered: boolean
+                        features: []
+                        contact: { name: string; email: string; phone: string }
+                        lastUpdate: string
+                    })
+                    this.populateModels()
+                }
+            },
+            error => {
+                if (error.status == 404) {
+                    this.router.navigate(['/error'])
+                }
             }
-        }, error => {
-            if (error.status == 404) {
-                this.router.navigate(['/error'])
-            }
-        });
+        )
+    }
+
+    private populateModels() {
+        const selectedMake = this.makes.find(
+            (x: { id: any }) => x.id == this.vehicle.makeId
+        )
+        this.models = selectedMake ? selectedMake.models : []
     }
 
     private setVehicle(v: Vehicle) {
-        this.vehicle.id = v.id;
-        this.vehicle.makeId = v.make.id;
-        this.vehicle.modelId = v.model.id;
-        this.vehicle.isRegistered = v.isRegistered;
-        this.vehicle.contact = v.contact;
-        this.vehicle.features = _.pluck(v.features, 'id');
+        this.vehicle.id = v.id
+        this.vehicle.makeId = v.make.id
+        this.vehicle.modelId = v.model.id
+        this.vehicle.isRegistered = v.isRegistered
+        this.vehicle.contact = v.contact
+        this.vehicle.features = _.pluck(v.features, 'id')
     }
 
     onMakeChange() {
-        const selectedMake = this.makes.find((x: { id: any; }) => x.id == this.vehicle.makeId);
-        this.models = selectedMake ? selectedMake.models : [];
-        delete this.vehicle.modelId;
+        this.populateModels()
+        delete this.vehicle.modelId
     }
 
-    onFeatureToggle(featureId: any, $event: { target: { checked: any; }; }) {
+    onFeatureToggle(featureId: any, $event: { target: { checked: any } }) {
         if ($event.target.checked) {
-            this.vehicle.features.push(featureId);
+            this.vehicle.features.push(featureId)
         } else {
-            const index = this.vehicle.features.indexOf(featureId);
-            console.log('Delete: ' + index);
-            this.vehicle.features.splice(index, 1);
+            const index = this.vehicle.features.indexOf(featureId)
+            console.log('Delete: ' + index)
+            this.vehicle.features.splice(index, 1)
         }
     }
 
     onSubmit() {
         this.vehicleService
             .createVehicle(this.vehicle)
-            .subscribe(result => result);
+            .subscribe(result => result)
     }
-
 }
